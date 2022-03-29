0e8ddfeecb9f13e087526e6f013b7f80c391a86f
zchananel <chananelz@gmail.com>
2022-03-29 12:12:07 +0300

    end ex1

--- Week3/homework_ex1_part1.R
+++ Week3/homework_ex1_part1.R
@@ -3,35 +3,59 @@ A$DateTime <- as.POSIXct( A$megawatthours, tz = "EST", "%H:%M EST %m/%d/%Y" )
 
 B <- A[ order(A$DateTime), ]
 
-mytabele = 
+
+# create data cube:
+BPAT_DF <- data.frame(time = B$DateTime, BPATNet = B$Net.generation )
+CISO_DF <- data.frame(time = B$DateTime,CISONet = B$Net.generation.1 )
+CPLE_DF <- data.frame(time = B$DateTime,CPLENet = B$Net.generation.2)
+ERCO_DF <- data.frame(time = B$DateTime,ERCONet = B$Net.generation.3 )
+FPL_DF <- data.frame(time = B$DateTime,FPLNet = B$Net.generation.4 )
+ISNE_DF <- data.frame(time = B$DateTime,ISNENet = B$Net.generation.5 )
+MISO_DF <- data.frame(time = B$DateTime,MISONet = B$Net.generation.6 )
+NYIS_DF <- data.frame(time = B$DateTime,NYISNet = B$Net.generation.7 )
+PACW_DF <- data.frame(time = B$DateTime,PACWNet = B$Net.generation.8 )
+PJM_DF <- data.frame(time = B$DateTime,PJMNet = B$Net.generation.9 )
+
+
+# convert the time to according the east cost
+start <- as.POSIXct("2021-02-07 00:00:00",tz = "EST")
+end <- as.POSIXct("2021-02-14 00:00:00",tz = "EST")
+
+BPAT_DF <-BPAT_DF[ which(BPAT_DF$time >= start -3*60*60 & BPAT_DF$time < end -3*60*60), ]
+CISO_DF <-CISO_DF[ which(CISO_DF$time >= start -3*60*60 & CISO_DF$time < end -3*60*60), ]
+CPLE_DF <-CPLE_DF[ which(CPLE_DF$time >= start & CPLE_DF$time < end), ] 
+ERCO_DF <-ERCO_DF[ which(ERCO_DF$time >= start -1*60*60 & ERCO_DF$time < end - 1*60*60), ]
+FPL_DF <- FPL_DF [ which(FPL_DF$time >= start & FPL_DF$time < end), ]
+ISNE_DF <-ISNE_DF[ which(ISNE_DF$time >= start & ISNE_DF$time < end), ]
+MISO_DF <-MISO_DF[ which(MISO_DF$time >= start -1*60*60 & MISO_DF$time < end -1*60*60), ]
+NYIS_DF <-NYIS_DF[ which(NYIS_DF$time >= start & NYIS_DF$time < end), ]
+PACW_DF <-PACW_DF[ which(PACW_DF$time >= start -3*60*60 & PACW_DF$time < end -3*60*60), ]
+PJM_DF <- PJM_DF [ which(PJM_DF$time >= start & PJM_DF$time < end), ]
+
+#creat df and clean the data
+netCube = cbind(PJM_DF,CISO_DF$CISONet,CPLE_DF$CPLENet,ERCO_DF$ERCONet,FPL_DF$FPLNet,ISNE_DF$ISNENet,MISO_DF$MISONet,NYIS_DF$NYISNet,PACW_DF$PACWNet,BPAT_DF$BPATNet)
+netCube <- unique(netCube)
+
+head(netCube)
 
 
 
-# analyze a time frame out of the power consumption data
-rng <- 816:983
-C <- with(B, cbind( Net.generation,Net.generation.1, Net.generation.2, Net.generation.3, Net.generation.4,Net.generation.5, 
-                    Net.generation.6, Net.generation.7 , Net.generation.8 , Net.generation.9 , Net.generation.10  ))
-C <- C[rng, ]
 
 
-# create an empty plot
-plot(1, type="n", xlab="", ylab="", xlim = c(0,12), ylim=c(-2, 2))
+netCube$avrege_gen <- c(1)
+netCube$avrege_gen <- rowMeans(netCube[,c('PJMNet','CISO_DF$CISONet','CPLE_DF$CPLENet','ERCO_DF$ERCONet','FPL_DF$FPLNet' ,'ISNE_DF$ISNENet','MISO_DF$MISONet','NYIS_DF$NYISNet','PACW_DF$PACWNet' )],na.rm = TRUE,dims = 1)
+netCube$myday <- format(netCube$time,"%d")
+daily_power_generation <-tapply(netCube$avrege_gen, netCube$myday,FUN=sum )
 
-DF <- data.frame(Time = rng - min(rng), Total.Net.Generation = NA)
-for( i in seq(168)){
-  DF[i,2] <- mean(C[ i,]) 
-}
 
-DF2 <- data.frame(Time = 1:7, Total.Net.Generation = NA)
-count <- 0
-for (i in seq(7)){
-  DF2[i,2] <- mean(DF[count:count+24,2])
-  count <- count +24  
-}
+
+
 
 pdf(file = "Week3/ex1_A.pdf",   # The directory you want to save the file in
     width = 5, # The width of the plot in inches
     height = 4)
-plot(DF2 , type ='b', main = "mean daily power generation across the US")
+plot(daily_power_generation,xaxt = "n", type ='b', xlab="day of the week", ylab="network gen",main = "mean daily power generation across the US")
+days <- c(7,8,9,10,11,12,13)
+axis(1, at=1:7, labels=days)
 dev.off()
 
---
+++ Week3/homework_ex1_part2.R
@@ -0,0 +1,139 @@
+A <- read.delim('Week3/table.tsv')
+A$DateTime <- as.POSIXct( A$megawatthours, tz = "EST", "%H:%M EST %m/%d/%Y" )
+
+B <- A[ order(A$DateTime), ]
+
+
+# step 1 create data cube:
+
+BPAT_DF <- data.frame(year = format(B$DateTime,"%Y") ,month = format(B$DateTime,"%m"), day = format(B$DateTime,"%d") ,hour = format(B$DateTime,"%H") , location = "BPAT" ,damand  = B$Demand )
+CISO_DF <- data.frame(year = format(B$DateTime,"%Y") ,month = format(B$DateTime,"%m"), day = format(B$DateTime,"%d") ,hour = format(B$DateTime,"%H") , location = "CISO" ,damand  = B$Demand.1 )
+CPLE_DF <- data.frame(year = format(B$DateTime,"%Y") ,month = format(B$DateTime,"%m"), day = format(B$DateTime,"%d") ,hour = format(B$DateTime,"%H") , location = "CPLE" ,damand  = B$Demand.2)
+ERCO_DF <- data.frame(year = format(B$DateTime,"%Y") ,month = format(B$DateTime,"%m"), day = format(B$DateTime,"%d") ,hour = format(B$DateTime,"%H") , location = "ERCO" ,damand  = B$Demand.3 )
+FPL_DF <-  data.frame(year = format(B$DateTime,"%Y") ,month = format(B$DateTime,"%m"), day = format(B$DateTime,"%d") ,hour = format(B$DateTime,"%H") , location = "FPL"  ,damand  = B$Demand.4 )
+ISNE_DF <- data.frame(year = format(B$DateTime,"%Y") ,month = format(B$DateTime,"%m"), day = format(B$DateTime,"%d") ,hour = format(B$DateTime,"%H") , location = "ISNE" ,damand  = B$Demand.5 )
+MISO_DF <- data.frame(year = format(B$DateTime,"%Y") ,month = format(B$DateTime,"%m"), day = format(B$DateTime,"%d") ,hour = format(B$DateTime,"%H") , location = "MISO" ,damand  = B$Demand.6 )
+NYIS_DF <- data.frame(year = format(B$DateTime,"%Y") ,month = format(B$DateTime,"%m"), day = format(B$DateTime,"%d") ,hour = format(B$DateTime,"%H") , location = "NYIS" ,damand  = B$Demand.7 )
+PACW_DF <- data.frame(year = format(B$DateTime,"%Y") ,month = format(B$DateTime,"%m"), day = format(B$DateTime,"%d") ,hour = format(B$DateTime,"%H") , location = "PACW" ,damand  = B$Demand.8 )
+PJM_DF <-  data.frame(year = format(B$DateTime,"%Y") ,month = format(B$DateTime,"%m"), day = format(B$DateTime,"%d") ,hour = format(B$DateTime,"%H") , location = "PJM"  ,damand  = B$Demand.9 )
+
+
+df_demand = rbind(BPAT_DF, CISO_DF, CPLE_DF, ERCO_DF, FPL_DF , ISNE_DF, MISO_DF, NYIS_DF, PACW_DF, PJM_DF )
+df_demand <- na.omit(df_demand) 
+
+demand_cube <- tapply(df_demand$damand ,
+                      df_demand[,c("year","month", "day","hour" ,"location")],
+                      FUN = mean )
+
+print(demand_cube)
+print (dimnames(demand_cube))
+
+# step 2 dice the cube and create cube of location = PJM, NYIS, ISNE, FPL, CPLE and time = 10:00-18:00, 20:00-03:00 
+
+dice_demand_cube <-
+  demand_cube[ ,
+               ,
+               ,
+               c("10" ,"11" ,"12" ,"13" ,"14" ,"15" ,"16" ,"17" ,"18"),
+               c("PJM", "NYIS", "ISNE", "FPL", "CPLE")]
+
+print(dice_demand_cube)
+print (dimnames(dice_demand_cube))
+
+# step 3 roll up the cube and create data frame of demand per hour:
+
+roll_up_demand_cube <- 
+  apply(dice_demand_cube, c("hour"),
+        FUN = function(x) mean(x, na.rm = TRUE) )
+
+demand_matrix = as.data.frame(roll_up_demand_cube)
+demand_matrix <- cbind(hour = rownames(demand_matrix), demand_matrix)
+rownames(demand_matrix) <- 1:nrow(demand_matrix)
+
+demand_vector = demand_matrix$roll_up_demand_cube
+demand_vector <- as.matrix(demand_vector)
+
+# step 4 calculate the linear regression according to the slides
+
+demand_matrix$hour <- c("0","1","2","3","4","5","6","7","8")
+
+
+U = data.frame(free = c(1) , hour = demand_matrix$hour)
+U <- as.matrix(sapply(U, as.numeric))
+U_tran = t(U)
+inverse_UU = solve(U_tran %*% U )
+UUU = inverse_UU %*% U_tran
+result <- UUU %*% demand_vector
+
+
+
+pdf(file = "Week3/ex2_A.pdf",   # The directory you want to save the file in
+    width = 6, # The width of the plot in inches
+    height = 4)
+plot(demand_matrix$hour, demand_matrix$roll_up_demand_cube, pch=19,xaxt = "n", xlab="hour", ylab="deman",main = "minute power demand in the east coast: 10:00-18:00")
+minits <- c(10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18)
+axis(1, at=0:8, labels=minits)
+abline(result[1], result[2])
+
+dev.off()
+
+
+
+# ------------------------------for 20:00 - 03:00 ---------------------------------
+
+dice_demand_cube <-
+  demand_cube[ ,
+               ,
+               ,
+               c("20","21","22","23","00","01","02","03"),
+               c("PJM", "NYIS", "ISNE", "FPL", "CPLE")]
+
+print(dice_demand_cube)
+print (dimnames(dice_demand_cube))
+
+# step 3 roll up the cube and create data frame of demand per hour:
+
+roll_up_demand_cube <- 
+  apply(dice_demand_cube, c("hour"),
+        FUN = function(x) mean(x, na.rm = TRUE) )
+
+demand_matrix = as.data.frame(roll_up_demand_cube)
+demand_matrix <- cbind(hour = rownames(demand_matrix), demand_matrix)
+rownames(demand_matrix) <- 1:nrow(demand_matrix)
+
+demand_vector = demand_matrix$roll_up_demand_cube
+demand_vector <- as.matrix(demand_vector)
+
+# step 4 calculate the linear regression according to the slides
+
+demand_matrix$hour <- c("0","1","2","3","4","5","6","7")
+
+U = data.frame(free = c(1) , hour = demand_matrix$hour)
+U <- as.matrix(sapply(U, as.numeric))
+U_tran = t(U)
+inverse_UU = solve(U_tran %*% U )
+UUU = inverse_UU %*% U_tran
+result <- UUU %*% demand_vector
+
+
+
+pdf(file = "Week3/ex2_B.pdf",   # The directory you want to save the file in
+    width = 6, # The width of the plot in inches
+    height = 4)
+plot(demand_matrix, xaxt = "n", xlab="hour", ylab="deman",main = "minute power demand in the east coast: 10:00-18:00")
+
+minits <- c(20,21,22,23,00,01,02,03)
+axis(1, at=0:7, labels=minits)
+abline(result[1], result[2])
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
